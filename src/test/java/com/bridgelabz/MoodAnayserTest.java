aa76830ef2c652c7537de34f39263d618fe86628
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-05-30 02:39:01 +0530

    Added:GiveGiven “GiveGiven “I am in Happy Mood” message in Constructor Should Return SAD

--- src/test/java/com/bridgelabz/MoodAnayserTest.java
+++ src/test/java/com/bridgelabz/MoodAnayserTest.java
@@ -14,6 +14,7 @@ public class MoodAnayserTest {
         Assertions.assertTrue(mood.equalsIgnoreCase("SAD"));
         // Verify that the actual mood matches the expected value "SAD"
     }
+
     @Test
     public void testAnalyseMood_AnyOtherMessage_ReturnsHappy() throws MoodAnalysisException {
         String message = "I am in Any Mood";
@@ -27,9 +28,10 @@ public class MoodAnayserTest {
         Assertions.assertEquals("HAPPY", mood);
         // Verify that the actual mood matches the expected value "HAPPY"
     }
+
     @Test
     public void testAnalyseMood_SadMessageInConstructor_ReturnsSad() {
-         String message = "I am in Sad Mood";
+        String message = "I am in Sad Mood";
         // The given message containing the word "Sad"
         MoodAnalyser moodAnalyser = new MoodAnalyser(message);
         // Create MoodAnalyser object with the given message
@@ -51,5 +53,31 @@ public class MoodAnayserTest {
 
         Assertions.assertEquals("SAD", mood);
         // Verify that the actual mood matches the expected value "SAD"
+    }
+
+    @Test
+    public void testAnalyseMood_HappyMessageInConstructor_ReturnsHappy() {
+        String message = "I am in Happy Mood";
+        // The given message containing the word "Happy"
+        MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+        // Create MoodAnalyser object with the given message
+
+        String mood = moodAnalyser.analyseMood();
+        // Call analyseMood method to get the actual mood
+
+        Assertions.assertEquals("HAPPY", mood);
+        // Verify that the actual mood matches the expected value "HAPPY"
+    }
+
+    @Test
+    public void testAnalyseMood_NoParameter_ReturnsHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        // Create MoodAnalyser object using the default constructor
+
+        String mood = moodAnalyser.analyseMood();
+        // Call analyseMood method to get the actual mood
+
+        Assertions.assertEquals("HAPPY", mood);
+        // Verify that the actual mood matches the expected value "HAPPY"
     }
 }
